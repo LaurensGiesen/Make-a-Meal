2a3d19d7c48627ea85e10e0ec9dbce390c29cc99
Laurens Giesen <laurens.giesen@student.howest.be>
2019-12-06 15:45:42 +0100

    tekst verandert mee bovenaan met de image --> random

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,10 +42,12 @@ function popUpDishes(){
     }
 
 
-function mealOfTheDay() {
-    let randomMeals = meals[Math.floor((Math.random() * meals.length))];
-    console.log(randomMeals);
-    let imageMeal = document.querySelector(`#mealoftheday`);
-    imageMeal.innerHTML += `<img src="images/${randomMeals.img}" alt="${randomMeals.title}" title="${randomMeals.title}">`;
+function mealOfTheDay(){
+   let randomMeals =  meals[Math.floor((Math.random() * meals.length))];
+   console.log(randomMeals);
+   let imageMeal = document.querySelector(`#mealoftheday`);
+   imageMeal.innerHTML += `<img src="images/${randomMeals.img}" alt="${randomMeals.title}" title="${randomMeals.title}">`;
+   let textMeal = document.querySelector(`#mealoftheday div p+p`);
+   textMeal.innerHTML += `<strong>${randomMeals.title}</strong>`;
 }
 
