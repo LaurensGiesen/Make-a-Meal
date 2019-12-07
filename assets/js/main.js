8668468060b6897144627e1e5254c7ec27c7ad7d
Laurens Giesen <laurens.giesen@student.howest.be>
2019-12-07 16:14:35 +0100

    info popup werkt bij elke image

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,13 +36,43 @@ function inLadenDishes() {
 function popUpDishes(e) {
     e.preventDefault();
     document.querySelector(`#popup`).classList.remove(`hidden`);
+
+     let selectedMealID = e.target.parentNode.parentNode.getAttribute(`data-id`) -1;
+         console.log(selectedMealID);
+        document.querySelector('#popup .contentwrapper').innerHTML =
+            `<article data-id="${meals[selectedMealID].id}">
+                <h3>${meals[selectedMealID].title}</h3>
+                <figure>
+                    <img src="images/${meals[selectedMealID].img}"
+                         alt="${meals[selectedMealID].title}"
+                         title="${meals[selectedMealID].title}">
+                    <figcaption>
+                        Meal by: <span>${meals[selectedMealID].cook}</span>
+                    </figcaption>
+                </figure>
+                <div class="info">
+                  <dl>
+                                <dt>calories:</dt>
+                                <dd>${meals[selectedMealID].calories}</dd>
+                                <dt>servings:</dt>
+                                <dd>${meals[selectedMealID].servings}</dd>
+                                <dt>days to book in advance:</dt>
+                                <dd>${meals[selectedMealID].book}</dd>
+                                <dt>type:</dt>
+                                <dd>${meals[selectedMealID].type}</dd>          
+                  </dl>
+
+                    <p>€ <span>${meals[selectedMealID].price}</span>/pp</p>
+                    <a href="#" class="order">Order</a>
+                </div>
+                
+            </article>`
 }
 
 
 function closePopUpDishes(e) {
     e.preventDefault();
     document.querySelector(`#popup`).classList.add(`hidden`);
-    console.log(`sluiten`);
 }
 
 function mealOfTheDay(){
