4a74ad3467086f988bb487e429e044cebca9bb3b
Laurens Giesen <laurens.giesen@student.howest.be>
2019-12-11 20:37:19 +0100

    searchfunctie werkt

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,12 +1,35 @@
 "use strict";
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded',init);
+let searchField;
 
-function init() {
-    document.querySelector(`#search`).addEventListener(`onkeypress`, search);
+function init(){
+    DOMelement();
+    setEventListeners();
+}
+function DOMelement(){
+    searchField =  document.querySelector('#search');
 }
 
-function search(e) {
-    e.preventDefault();
+function setEventListeners(){
+    searchField.addEventListener("keyup", SearchList);
+}
+function SearchList(){
+    let length = meals.length;
+    let searchString = searchField.value;
 
+    for(let i = 0; i < length; i++){
+        let txtValue = meals[i].title.toLowerCase();
+        if(txtValue.indexOf(searchString) > -1){
+            let displayNone;
+            let showId = i + 1;
+            displayNone = document.querySelector("[data-id='"+ showId + "'");
+            displayNone.style.display = "";
+        } else{
+            let displayNone;
+            let verwijderId = i + 1;
+            displayNone = document.querySelector("[data-id='"+ verwijderId + "'");
+            displayNone.style.display = "none";
+        }
+    }
 }
