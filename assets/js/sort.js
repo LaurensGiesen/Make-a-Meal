85958bc15bbd11f1b43889b3c8f02bcc7db6da9c
Laurens Giesen <laurens.giesen@student.howest.be>
2019-12-13 18:11:46 +0100

    sortfunctie werkt op de mealProperty

--- assets/js/sort.js
+++ assets/js/sort.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', init6);
 
 function init6() {
     inLadenValues();
+    document.querySelector('#sortby').addEventListener('change', mealProperty);
 }
 function inLadenValues() {
     document.querySelector(`#sortby`).innerHTML =
@@ -17,4 +18,23 @@ function inLadenValues() {
          <option value="servings">servings</option>
          <option value="title">title</option>
          <option value="type">type</option>`;
+}
+
+function mealProperty(e) {
+    let selectedProperty = e.target.value;
+    let ascOrDesc = document.querySelector('#direction').selectedIndex;
+    meals.sort(function (a, b) {
+        let propertyA = a[selectedProperty];
+        let propertyB = b[selectedProperty];   
+
+        if(propertyA < propertyB){
+            return -1;
+        }
+        if(propertyA > propertyB){
+            return 1;
+        }
+        return 0;
+    });
+    if(ascOrDesc === 1){ meals.reverse()}
+    inLadenDishes(meals);
 }
